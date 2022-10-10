23bc49a630acf451e3b63c3be52405dde1fc4c10
Teshima <takuto0418340@gmail.com>
2022-10-10 23:42:51 +0900

    #3修正：プルリクエストで指摘された点を修正しました

--- Assets/Scripts/AppearObject.cs
+++ Assets/Scripts/AppearObject.cs
@@ -10,7 +10,7 @@ public class AppearObject : MonoBehaviour
     [SerializeField]
     private GameObject appearObject;
     [SerializeField]
-    private float AppearInterval;
+    private float appearIntervalSeconds;
 
     private const float MinXPosition = -5.0f;
     private const float MaxXPosition = 5.0f;
@@ -25,15 +25,21 @@ public class AppearObject : MonoBehaviour
         StartCoroutine (Appear());
     }
 
+    ///<summary>
+    ///一定間隔で落下物の生成を行うコルーチン
+    ///</summary>
     private IEnumerator Appear() 
     {
         while(true)
         {
-            yield return new WaitForSeconds (AppearInterval);
+            yield return new WaitForSeconds (appearIntervalSeconds);
             RandomAppear();
         }
     }
     
+    ///<summary>
+    ///ランダムに落下物を生成するメソッド
+    ///</summary>
     void RandomAppear()
     {
         appearXPosition = Random.Range(MinXPosition,MaxXPosition);
--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+///<summary>
+///制限時間を表示するクラス
+///</smmary>
 public class TimerController : MonoBehaviour
 {
     [SerializeField]
     private Text timerText;
+    [SerializeField]
+    private  float limitSeconds;
 
-    private  float LimitTime = 31;
-    private int seconds;
+    private float seconds;
 
     void FixedUpdate()
     {
-        LimitTime -= Time.deltaTime;
-        seconds = (int)LimitTime;
-        timerText.text = seconds.ToString(); 
+        limitSeconds -= Time.deltaTime;
+        seconds = limitSeconds;
+        timerText.text = $"{seconds:F0}"; 
         if(seconds <= 0){
             Time.timeScale = 0;
         }
